5964d23092a06ee8edc7dea38be8f1beb5c7b41d
Alexandre Lopes <alexandrefernadeslopes@gmail.com>
2025-10-07 10:25:33 +0100

    added bubbles

--- pw2/MyContents.js
+++ pw2/MyContents.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { MyAxis } from './MyAxis.js';
+import { MyBubble } from './MyBubble.js';
 
 /**
  *  This class contains the contents of out application
@@ -19,6 +20,7 @@ class MyContents  {
     init() {
         this.initLights();
         this.initObjects();
+        this.initTextures();
     }
 
     initLights() {
@@ -37,16 +39,28 @@ class MyContents  {
         }
 
         const floorGeometry = new THREE.PlaneGeometry(10, 10);
-        const floorMaterial = new THREE.MeshPhongMaterial({map: this.floorTexture, color: "#ffffffff", shininess: 2000, specular: "#ffffff"});
+        const floorMaterial = new THREE.MeshPhongMaterial({map: this.floorTexture, color: "#ad8554", shininess: 2000, specular: "#ffffff"});
         this.planeMesh = new THREE.Mesh(floorGeometry, floorMaterial);
         this.planeMesh.rotation.x = -Math.PI / 2;
         this.planeMesh.position.y = -0;
         this.app.scene.add(this.planeMesh);
 
-    }   
+        this.bubbles = [];
+        for (let i = 0; i < 10; i++) {
+            const bubble = new MyBubble(0.10 + Math.random() * 0.08, 1); 
+            this.app.scene.add(bubble);
+            this.bubbles.push(bubble);
+        }
 
-    update(){
+    }
 
+    initTextures() {
+        
+    }
+
+    update(delta) {
+        if (!delta) return;
+        for (const b of this.bubbles) b.update(delta);
     }
 }
 
